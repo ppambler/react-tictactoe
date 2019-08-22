8cc643ef2638c8eaaf4f923b082e9f8ebfc31e7d
Ambler <allinlbj@gmail.com>
2019-08-22 22:51:17 +0800

    编译前的简写姿势

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// const div = (
+//   React.createElement('div', null,
+//     React.createElement('p', null,
+//       React.createElement('span', null, '我是一个span')))
+// )
+
 const div = (
-  React.createElement('div', null,
-    React.createElement('p', null,
-      React.createElement('span', null, '我是一个span')))
+  <div>
+    <p>
+      <span>我是一个span</span>
+    </p>
+  </div>
 )
 
 console.log(div) //element 是个虚拟的DOM，类型是对象
