0084f896078a39eb5e49b25de209e2f1ebf193f5
Ambler <allinlbj@gmail.com>
2019-08-22 16:10:22 +0800

    div2 到 div 的翻译过程

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-const div = createElement('div', 
-                createElement('p', 
-                  createElement('span', '我是一个span')))
-                                  
+const div = (
+  t('div',
+    t('p',
+      t('span', '我是一个span')))
+)
+
+const div2 = (
+  <div>
+    <p>
+      <span>'我是一个span'</span>
+    </p>  
+  </div>
+)
+
 
 document.body.appendChild(div)
 
-function createElement(tagName, children) {
+function t(tagName, children) {
   const element = document.createElement(tagName)
-  if(children) {
-    if(typeof children === 'string') {
+  if (children) {
+    if (typeof children === 'string') {
       let childrenTextNode = document.createTextNode(children)
       element.appendChild(childrenTextNode)
     } else {
