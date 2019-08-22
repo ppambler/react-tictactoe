221c347e0973a1a84a186637451475e93d40ff83
Ambler <allinlbj@gmail.com>
2019-08-22 10:20:09 +0800

    使用原生JS  构造 一个简单的DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+console.log('hi')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const div = document.createElement('div')
+const p = document.createElement('p')
+const span = document.createElement('span')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+div.appendChild(p)
+p.appendChild(span)
+span.innerText = '我是一个span'
+
+document.body.appendChild(div)
