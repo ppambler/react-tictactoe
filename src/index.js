f8c907bd87fb284996cabc0ca234782fd8f02311
Ambler <allinlbj@gmail.com>
2019-08-22 10:32:50 +0800

    封装一个 createElement 函数，只有一个 tagName 参数

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 console.log('hi')
 
-const div = document.createElement('div')
-const p = document.createElement('p')
-const span = document.createElement('span')
+const div = createElement('div')
+const p = createElement('p')
+const span = createElement('span')
 
 div.appendChild(p)
 p.appendChild(span)
 span.innerText = '我是一个span'
 
 document.body.appendChild(div)
+
+function createElement(tagName) {
+  return document.createElement(tagName)
+}
