23381f8e6269004250bae3c60730cfa32c2f685c
Ambler <allinlbj@gmail.com>
2019-08-23 16:16:41 +0800

    井字棋 功能 基本实现

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css'
 
 
-const cells = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-]
+
 
 const Cell = function (props) {
   return (
-    <div className="cell">
+    <div className="cell" onClick={props.onClick}>
       {props.text}
     </div>
   )
 }
 
 const Chessboard = function () {
+
+  const [cells, setCells] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ])
+  const [n, setN] = useState(0)
+  const tell = () => {
+    console.log('tell 判断谁赢了')
+  }
+  const onClickCell = (row, col) => {
+    console.log('click')
+    setN(n + 1)
+    const copy = JSON.parse(JSON.stringify(cells))
+    copy[row][col] = n % 2 === 0 ? 'x' : 'o'
+    setCells(copy)
+    tell()
+  }
   return (
     <div>
+      <div>n:{n}</div>
       {
-        cells.map(items =>
+        cells.map((items, row) =>
           <div className="row">
-            {items.map(item =>
+            {items.map((item, col) =>
               <div className="col">
-                <Cell text={item} />
+                <Cell text={item} onClick={() => onClickCell(row, col)} />
               </div>
             )}
           </div>
