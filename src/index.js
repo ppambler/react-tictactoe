158003513a5ffc3d8ebe79b6c1e089e345b0edc9
Ambler <allinlbj@gmail.com>
2019-08-25 03:09:31 +0800

    让内置的微信浏览器支持 window.location.reload()

--- src/index.js
+++ src/index.js
@@ -99,13 +99,23 @@ const Chessboard = function () {
         finished &&
         <div className="gameOver">
           {result}
-          <svg className="icon again" onClick={() => window.location.reload()} aria-hidden="true"><use xlinkHref="#icon-again"></use></svg>
+          <svg className="icon again" onClick={() => refresh()} aria-hidden="true"><use xlinkHref="#icon-again"></use></svg>
         </div>
       }
     </div>
   )
 }
 
+function refresh() {
+  let random = Math.floor((Math.random() * 10000) + 1);
+  let url = decodeURI(window.location.href);
+  if (url.indexOf('?') < 0) {
+    url = url + "?random" + random;
+  } else {
+    url = url.substr(0, url.indexOf('?random')) + "?random" + random;
+  }
+  window.location.href = url;
+}
 
 ReactDOM.render(<div>
   <Chessboard />
