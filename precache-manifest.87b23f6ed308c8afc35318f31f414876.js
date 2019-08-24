ac6fbb5b629929a9ac689ffbef21a991226b8afe
Ambler <allinlbj@gmail.com>
2019-08-25 03:07:48 +0800

    deploy

---
+++ precache-manifest.87b23f6ed308c8afc35318f31f414876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456011f830d495ec3f02a04ef1f84cc5",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "f0fb8916ef269c7a9b9d",
+    "url": "/react-tictactoe/static/css/main.0c506789.chunk.css"
+  },
+  {
+    "revision": "c442aac842adc3aae3d4",
+    "url": "/react-tictactoe/static/js/2.32264029.chunk.js"
+  },
+  {
+    "revision": "f0fb8916ef269c7a9b9d",
+    "url": "/react-tictactoe/static/js/main.85120dde.chunk.js"
+  },
+  {
+    "revision": "a314270d61c966fd3661",
+    "url": "/react-tictactoe/static/js/runtime~main.4cd99ab2.js"
+  }
+]);
